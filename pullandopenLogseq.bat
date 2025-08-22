aa0cda3266e45cbc2a1fd7562449075978f1206f
248lee <fantasy10final@gmail.com>
2025-08-22 09:21:04 +0800

    commit by running script

--- pullandopenLogseq.bat
+++ pullandopenLogseq.bat
@@ -1,6 +1,6 @@
 git fetch
 git pull
-C:\Users\fanta\AppData\Local\Logseq\Logseq.exe
+start /wait "" "C:\Users\fanta\AppData\Local\Logseq\Logseq.exe"
 git add --all
 git commit -m "commit by running script"
 git fetch
